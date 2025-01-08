0821160c947d272c58a765842ce848369955da86
João Pedro Rocha <jprocha@Joaos-MacBook-Pro-3.local>
2025-01-08 10:24:32 -0300

    Fized dropdown and contact icons

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,24 @@
 import React from 'react'
+import '../styles/navbar.css'
+
 
 const header = () => {
     return (
         <div> 
-            <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-            <div className="container">
-                <a className="navbar-brand" href="#page-top">Start Bootstrap</a>
-                <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Menu
-                <i className="fas fa-bars" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav ms-auto">
-                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Portfolio</a></li>
-                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#about">About</a></li>
-                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contact</a></li>
-                </ul>
+            <nav className="navbar navbar-expand-lg bg-secondary fixed-top" id="mainNav">
+                <div className="container"><a className="navbar-brand js-scroll-trigger" href="#page-top">START BOOTSTRAP</a>
+                    <button className="navbar-toggler navbar-toggler-right font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu <i className="fas fa-bars" /></button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ml-auto text-center text-lg-left">
+                        <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">PORTFOLIO</a>
+                        </li>
+                        <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">ABOUT</a>
+                        </li>
+                        <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">CONTACT</a>
+                        </li>
+                    </ul>
+                    </div>
                 </div>
-            </div>
             </nav>
         </div>
     )
